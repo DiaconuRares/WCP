0ac6e07074b16ce4796633927348b906c55a540e
DiaconuRares <40871918+DiaconuRares@users.noreply.github.com>
2018-11-04 12:50:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+    $(".links>.link0").click( function() {
+        window.location.href = "https://diaconurares.github.io/WCP/despre.html";
+    });
     
+    $(".links>.link1").click( function() {
+        window.location.href = "https://diaconurares.github.io/WCP/cumpara.html";
+    });
+    
+    $(".links>.link2").click( function() {
+        window.location.href = "https://diaconurares.github.io/WCP/contact.html";
+    });
 });
